40a644b9e22923179d9ef7ad49e095b6c1960e84
Sarthak Aggarwal <sarthak10082001@gmail.com>
2024-01-29 10:48:42 +0530

    second commit

---
+++ jaeger-server/src/main/java/com/sarthak/jaegerserver/service/JaegerServerService.java
@@ -0,0 +1,24 @@
+package com.sarthak.jaegerserver.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class JaegerServerService {
+
+  private WebClient webClient;
+
+  public JaegerServerService(WebClient webClient) {
+    this.webClient = webClient;
+  }
+
+  public Mono<String> get(Integer id) {
+
+    return webClient.get()
+        .uri("http://numbersapi.com/" + id)
+        .retrieve()
+        .bodyToMono(String.class);
+  }
+}
